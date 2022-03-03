abc8211afeba44b4b0be471105dfb7d5dddd6326
cell gonzales <aragonzales.k@gmail.com>
2022-03-02 16:38:21 -0800

    create dynamic variables for requesting logic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,13 @@
-var getUserRepos = function(){
-    fetch('https://api.github.com/users/octocat/repos')
-};
+//set parameter for the function
+var getUserRepos = function(user){
+    //format the github api url. insert parameter to the api URL
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    //make request to url -- using formatted URL from apiURL
+    fetch(apiUrl).then(function(response){
+    response.json().then(function(data){
+        console.log(data);
+    });
+});
+}
 getUserRepos();
